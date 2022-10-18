e1c46cbfabb336d370394f665d6750ef49ecce9a
Mairon Lucas <maironslusarz@tokenlab.com.br>
2022-10-17 21:35:39 -0300

    Add Firebase core dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_task_list/common/tls_general_provider.dart';
 import 'package:flutter_task_list/config.dart';
 import 'package:flutter_task_list/main_container_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
